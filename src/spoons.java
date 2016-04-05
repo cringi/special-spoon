eb0dadeec4da40608469bad11a2e490f4b4ba2df
Brandon Berney <brandonberney@protonmail.com>
2016-04-05 10:33:22 -0400

    hey

--- src/spoons.java
+++ src/spoons.java
@@ -1,5 +1,27 @@
-public class spoons {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class spoons {
+    private static ArrayList<String> names = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        do {
+            String value = input.next();
+            value = value.toLowerCase();
+
+            if (value.equals("stop")) {
+                break;
+            }
+            else {
+                names.add(titleCase(value));
+            }
+        }while (true);
+
+        System.out.println(names);
+    }
+
+    private static String titleCase(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 }
